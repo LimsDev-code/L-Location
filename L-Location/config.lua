f81261a53b1824814723e44ee2c3882df3284f1f
Lims <chaputclement83@gmail.com>
2025-04-02 14:00:09 +0200

    Update config.lua

--- L-Location/config.lua
+++ L-Location/config.lua
@@ -1,25 +1,25 @@
 Config = {}
 
--- Coordonnées pour l'ouverture du menu
+-- Coordinates for opening the menu
 Config.MenuLocation = {
     x = -1032.9912,
     y = -2734.5378,
     z = 20.1693,
-    radius = 2.0 -- Rayon pour activer le menu
+    radius = 2.0 -- Radius to activate the menu
 }
 
--- Emplacements pour le spawn des véhicules
+-- Locations for vehicle spawning
 Config.VehicleSpawnLocations = {
     { x = -1029.4673, y = -2731.0149, z = 20.1492, heading = 239.7009 },
     { x = -1035.0303, y = -2728.6294, z = 20.1128, heading = 244.4818 },
     { x = -1041.1661, y = -2724.8958, z = 20.1208, heading = 245.1415 }
 }
 
--- Coordonnées et modèle du ped
+-- Coordinates and model of the ped
 Config.PedLocation = {
-    model = "a_m_y_business_01", -- Modèle du ped
+    model = "a_m_y_business_01", -- Ped model
     x = -1032.5664,
     y = -2734.1086,
     z = 20.1693,
-    heading = 146.2182 -- Orientation du ped
+    heading = 146.2182 -- Ped orientation
 }
